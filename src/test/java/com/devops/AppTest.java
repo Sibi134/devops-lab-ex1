689b49e8581edb315ea96da31c41ba53478678b0
Sibi134 <sibi.20040000@gmail.com>
2026-02-04 10:06:17 +0530

    Create AppTest.java

---
+++ src/test/java/com/devops/AppTest.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
